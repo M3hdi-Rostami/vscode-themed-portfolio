db76a3d5f67872b2eaaadb73c37b77149fec2130
M3hdi-Rostami <m3hdi.rostami@gmail.com>
2025-07-04 12:50:12 +0330

    Feat: Add fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import type { ReactNode } from "react";
+import localFont from "next/font/local";
 import "@/app/globals.css";
 
 export const metadata: Metadata = {
@@ -7,12 +8,28 @@ export const metadata: Metadata = {
   description: "Portfolio as mehdi rostami",
 };
 
+const UbuntuSans = localFont({
+  src: [
+    {
+      path: "./fonts/UbuntuSans/UbuntuSans-Regular.ttf",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "./fonts/UbuntuSans/UbuntuSans-Bold.ttf",
+      weight: "700",
+      style: "normal",
+    },
+  ],
+  variable: "--font-ubuntu-sans",
+});
+
 export default function RootLayout({
   children,
 }: Readonly<{ children?: ReactNode }>) {
   return (
     <html lang="fa" dir="rtl" suppressHydrationWarning>
-      <body className={`min-h-screen antialiased`}>
+      <body className={`min-h-screen ${UbuntuSans.className} antialiased`}>
         <main>{children}</main>
       </body>
     </html>
